ae88be9cdce98f7e7e5474f8cb0a44f6f3d39475
legoamypond <legoamypond@gmail.com>
2025-09-25 13:39:27 +0000

    MyFirstCommit

---
+++ src/com/Camp.java
@@ -0,0 +1,24 @@
+package com.mycamptest;
+public class Camp {
+
+//atributes fields
+
+public String campName; 
+
+//constructor method
+
+public Camp(String campName){
+    this.campName = campName;
+
+}
+
+//behavoirs
+public void getCamp(){
+    System.out.println("Welcome to "+ campName);
+}
+
+
+
+
+
+} 
---
+++ src/com/Main.java
@@ -0,0 +1,11 @@
+package com.mycamptest;
+public class Main{
+
+    public static void main(String[] args){
+        Camp newCamp = new Camp("4H Cook camp")
+        newCamp.getCamp();
+
+        Counselor newCounselor = new Counselor("Sydney",15,"4H Cook camp");
+        newCounselor.intro();
+    } 
+}
